d4e88526cffdd6e8ff817666ca8e1617bd983641
wuqr <wql789@qq.com>
2021-08-15 12:41:24 +0800

    loadBalanced restTemplate默认的负载均衡机制

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/config/ApplicationContextConfig.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/config/ApplicationContextConfig.java
@@ -13,6 +13,7 @@ public class ApplicationContextConfig {
     @Bean
     @LoadBalanced // 以前你给我rest template一个url我直接请求就好了， 现在你给我的是服务名称，不再写死的 url你要给我一个负载均衡机制，我才
     // 知道多个节点下去请求哪个节点 。 默认应该就是轮询
+    // loadBalance 加载平衡，平均。就是分发呗
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
