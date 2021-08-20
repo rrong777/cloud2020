27d6ca7f08edb340eed4cb7f537a10e975fa7f4c
wuqr <wql789@qq.com>
2021-08-20 22:53:18 +0800

    测试ribbon

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.atguigu.springcloud.controller;
 import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,5 +37,33 @@ public class OrderController {
     public CommonResult<Payment> getPayment(@PathVariable("id") Long id)  {
         // 发起一个get请求
         return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+    }
+    @GetMapping("/payment/getForEntity/{id}")
+    public CommonResult<Payment> getPayment2(@PathVariable("id") Long id)  {
+        // 进入getForObject参数一样。两个参数，一个是uri 一个是响应体的类型（响应给你的数据是什么类型 ）
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL + "/payment/getForEntity/" + id, CommonResult.class);
+        log.info(entity.toString());
+        if(entity.getStatusCode().is2xxSuccessful()) {
+            return entity.getBody();// 如果响应状态码是2开头，表示成功
+        } else {
+            return new CommonResult<>(444, "请求失败 ");
+        }
+    }
+
+    /**
+     * 浏览器发起一个请求到这里，这里发起一个postForEntity请求测试 响应
+     * @param payment
+     * @return
+     */
+    @GetMapping("/payment/createForPostForEntity")
+    public CommonResult<Payment> createForPostForEntity(Payment payment)  {
+        // 三个参数 url paramter 返回值类型 发起一个post请求
+        ResponseEntity<CommonResult> entity = restTemplate.postForEntity(PAYMENT_URL + "/payment/createForPostForEntity", payment, CommonResult.class);
+        log.info(entity.toString());
+        if(entity.getStatusCode().is2xxSuccessful())  {
+            return entity.getBody();
+        } else {
+            return new CommonResult<>(444, "请求失败");
+        }
     }
 }
--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -47,6 +47,27 @@ public class PaymentController {
             return new CommonResult(444, "没有id为：" + id + "的对应记录", null);
         }
     }
+    @PostMapping("/createForPostForEntity")
+    public CommonResult createForPostForEntity(@RequestBody Payment payment){
+        int result = paymentService.create(payment);
+        log.info("*******插入结果：" + result + "serverPost:" + serverPost);
+        if(result  > 0) {
+            return new CommonResult(200, "插入数据库成功", result);
+        } else {
+            return new CommonResult(444, "插入数据库失败", null);
+        }
+    }
+
+    @GetMapping("/getForEntity/{id}")
+    public CommonResult<Payment> getForEntity(@PathVariable("id") Long id){
+        Payment payment = paymentService.getPaymentById(id);
+        log.info("*******查询结果：" + payment);
+        if(payment  != null) {
+            return new CommonResult(200, "查询成功", payment + "serverPost:" + serverPost);
+        } else {
+            return new CommonResult(444, "没有id为：" + id + "的对应记录", null);
+        }
+    }
 
     @GetMapping("/discovery")
     public Object discovery(){
