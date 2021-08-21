1ae6663d9ab41392a1f1d95b3e4716cf81b39584
wuqr <wql789@qq.com>
2021-08-21 10:10:57 +0800

    恢复ribbon默认的负载均衡策略

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
@@ -11,7 +11,7 @@ import org.springframework.cloud.netflix.ribbon.RibbonClient;
 // 告诉SpringBoot，我要使用的ribbon 负载策略，这里替换掉默认的
 // name = "CLOUD-PAYMENT-SERVICE", 我当前这个服务要去访问的服务
 // 我这个配置 覆盖掉 默认提供的轮询负载均衡机制
-@RibbonClient(name = "CLOUD-PAYMENT-SERVICE",configuration = MySelfRule.class)
+//@RibbonClient(name = "CLOUD-PAYMENT-SERVICE",configuration = MySelfRule.class)
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
