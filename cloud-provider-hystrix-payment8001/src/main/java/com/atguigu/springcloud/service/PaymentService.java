10041089613e7907663c95e2e89e5cafcfe08647
wuqr <wql789@qq.com>
2021-08-28 17:04:44 +0800

    服务熔断

--- cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
@@ -1,8 +1,10 @@
 package com.atguigu.springcloud.service;
 
+import cn.hutool.core.util.IdUtil;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.concurrent.TimeUnit;
 @Service
@@ -32,4 +34,27 @@ public class PaymentService {
     public String paymentInfo_TimeOutHandler(Integer id){
         return "线程池："+Thread.currentThread().getName()+"   系统繁忙，请稍后重试！,id：  "+id+"\t"+"/(ㄒoㄒ)/~~";
     }
+
+
+    //服务熔断
+    @HystrixCommand(fallbackMethod = "paymentCircuitBreaker_fallback",commandProperties = {
+            @HystrixProperty(name = "circuitBreaker.enabled",value = "true"),  //是否开启断路器
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold",value = "10"),   //请求次数
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds",value = "10000"),  //时间范围
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage",value = "60"), //失败率达到多少后跳闸
+    }) // 上面四个配置的意思是，假设在10000ms内，十次请求有60%的 概率是失败的之后就打开保险丝（断路器）触发熔断
+    // 上面就是一个微服务接口
+    public String paymentCircuitBreaker(@PathVariable("id") Integer id){
+        if (id < 0){
+            throw new RuntimeException("*****id 不能负数");
+        }
+        String serialNumber = IdUtil.simpleUUID();
+
+        return Thread.currentThread().getName()+"\t"+"调用成功,流水号："+serialNumber;
+    }
+    // 下面就是一个服务降级的兜底方法
+    public String paymentCircuitBreaker_fallback(@PathVariable("id") Integer id){
+        return "id 不能负数，请稍候再试,(┬＿┬)/~~     id: " +id;
+    }
+
 }
