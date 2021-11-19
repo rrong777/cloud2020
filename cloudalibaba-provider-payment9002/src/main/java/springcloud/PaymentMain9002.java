15cfafe51d6ae5e5e36f96042675fdfee386cb51
rrong777 <wql789@qq.com>
2021-11-19 20:49:22 +0800

    新建9002连接nacos

---
+++ cloudalibaba-provider-payment9002/src/main/java/springcloud/PaymentMain9002.java
@@ -0,0 +1,23 @@
+package springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author wql78
+ * @title: PaymentMain9001
+ * @description: @TODO
+ * @date 2021-11-19 20:30:43
+ */
+
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class PaymentMain9002 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9002.class,args);
+    }
+}
+
+
