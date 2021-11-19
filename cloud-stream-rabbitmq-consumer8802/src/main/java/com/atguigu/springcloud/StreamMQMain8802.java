97a43ee6de28e6db9ef4ad360c81fdb6213c0c2b
rrong777 <wql789@qq.com>
2021-11-19 17:31:01 +0800

    SpringCloudStream整合RabbitMQ的消息生产者

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/atguigu/springcloud/StreamMQMain8802.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wql78
+ * @title: StreamMQMain8802
+ * @description: @TODO
+ * @date 2021-11-19 17:19:22
+ */
+@SpringBootApplication
+public class StreamMQMain8802 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8802.class, args);
+    }
+}
---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/atguigu/springcloud/controller/ReceiveMessageListenerController.java
@@ -0,0 +1,28 @@
+package com.atguigu.springcloud.controller;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author wql78
+ * @title: ReceiveMessageListenerController
+ * @description: @TODO
+ * @date 2021-11-19 17:20:30
+ */
+@Component
+@EnableBinding(Sink.class)
+public class ReceiveMessageListenerController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @StreamListener(Sink.INPUT) // 监听的是输入源
+    public void input(Message<String> message) {
+        // 发送的时候 output.send(MessageBuilder.withPayload(serial).build()); 是build一个Message对象，接收的时候也应该是一个Message对象
+        System.out.println("消费者1号，------------->接收到的消息" + message.getPayload() + "\tport:" + serverPort);
+    }
+}
