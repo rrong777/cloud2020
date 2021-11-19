f3776983024e073a4d7015e4233a27cea33a4afc
rrong777 <wql789@qq.com>
2021-11-19 17:46:44 +0800

    克隆8802 生成一个8803组

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/springcloud/StreamMQMain8803.java
@@ -0,0 +1,17 @@
+package springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wql78
+ * @title: StreamMQMain8802
+ * @description: @TODO
+ * @date 2021-11-19 17:19:22
+ */
+@SpringBootApplication
+public class StreamMQMain8803 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8803.class, args);
+    }
+}
